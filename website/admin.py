bdc8a57c44e1ea64a942918d41e8fb2658f5681d
Edghi <ghislain.makolle@facsciences-uy1.cm>
2022-08-30 17:48:34 +0100

    Adding models

--- website/admin.py
+++ website/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
 
+
 # Register your models here.
+from website.models import Project, Resume
+
+
+class ProjectAdmin(admin.ModelAdmin):
+    list_display = ('name', 'url', 'project_type')
+    list_filter = ('project_type',)
+    search_fields = ('name',)
+
+
+class ResumeAdmin(admin.ModelAdmin):
+    list_display = ("title", "structure", "resume_type")
+    list_filter = ("resume_type",)
+    search_fields = ("title", "structure")
+
+
+admin.site.register(Project, ProjectAdmin)
+admin.site.register(Resume, ResumeAdmin)
