5b641cd57a27a6352fb420c12c605ff721129a20
Edghi <ghislain.makolle@facsciences-uy1.cm>
2022-08-30 19:30:13 +0100

    Adding data on the html template

--- website/views.py
+++ website/views.py
@@ -1,12 +1,18 @@
 from django.shortcuts import render
 from django.views.generic import View
 
+from website.models import Project, Resume
+
 
 class HomeView(View):
     template_name = "index.html"
 
     def get(self, request, *args, **kwargs):
-        return render(request, self.template_name, {})
+        projects = Project.objects.all()
+        communities = Resume.objects.filter(resume_type="Community").order_by("-id")
+        professionals = Resume.objects.filter(resume_type="Professional").order_by("-id")
+        context = {"projects": projects, "communities": communities, "professionals": professionals}
+        return render(request, self.template_name, context)
 
     def post(self, request, *args, **kwargs):
         ...
