543c60905199e6de906f4c3b997e198c5fb27b2d
EdGhi <ghislain.makolle@facsciences-uy1.cm>
2023-02-06 18:26:40 +0100

    Reinitialize the project

--- website/models.py
+++ website/models.py
@@ -1,38 +1,5 @@
 from django.db import models
 
-PROJECT_TYPES = [
-    ("App", "Application"),
-    ("Scripts", "Scripts"),
-    ("Web", "Website"),
-]
-
-RESUME_TYPES = [
-    ("Community", "Community"),
-    ("Professional", "Professional"),
-]
-
-
-# Create your models here.
-class Project(models.Model):
-    name = models.CharField(max_length=100, blank=False, null=False)
-    url = models.CharField(max_length=255, blank=False, null=False)
-    project_type = models.CharField(choices=PROJECT_TYPES, max_length=50)
-
-    def __str__(self):
-        return self.name
-
-
-class Resume(models.Model):
-    title = models.CharField(max_length=50, blank=False, null=False)
-    start = models.CharField(max_length=50, blank=False, null=False)
-    end = models.CharField(max_length=50, blank=False, null=False)
-    structure = models.CharField(max_length=100, blank=False, null=False)
-    description = models.TextField()
-    resume_type = models.CharField(choices=RESUME_TYPES, max_length=50)
-
-    def __str__(self):
-        return self.title
-
 
 class Cv(models.Model):
     file = models.FileField(upload_to='resumes/')
--- website/views.py
+++ website/views.py
@@ -1,17 +1,1 @@
 from django.shortcuts import render
-from django.views.generic import View
-
-from website.models import Project, Resume, Cv
-
-
-class HomeView(View):
-    template_name = "index.html"
-
-    def get(self, request, *args, **kwargs):
-        cv = Cv.objects.all().first()
-        projects = Project.objects.all()
-        communities = Resume.objects.filter(resume_type="Community").order_by("-id")
-        professionals = Resume.objects.filter(resume_type="Professional").order_by("-id")
-        context = {"projects": projects, "communities": communities, "professionals": professionals, "cv": cv}
-
-        return render(request, self.template_name, context)
