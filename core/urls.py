8dfb0eb2375e9b2aaa54d3c43ffa23fd2adddbbd
joelfah <joelfah2003@gmail.com>
2023-03-02 20:10:43 +0100

    feat: Added website UI

--- core/urls.py
+++ core/urls.py
@@ -17,9 +17,12 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
+from website.views import page_not_found_view
 
 urlpatterns = [
-                  path("admin/", admin.site.urls),
-                  path("", include("website.urls")),
-              ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) + static(settings.MEDIA_URL,
-                                                                                           document_root=settings.MEDIA_ROOT)
+    path("admin/", admin.site.urls),
+    path("", include("website.urls")),
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+
+handler404 = page_not_found_view.as_view()
--- website/urls.py
+++ website/urls.py
@@ -1,6 +1,8 @@
-from django.urls import path, include
+from django.urls import path
+from .views import HomeView, CaseView
 
 
 urlpatterns = [
-
+    path('', HomeView.as_view(), name="home"),
+    path('details/<str:pk>/<slug:project>/', CaseView.as_view(), name="case"),
 ]
