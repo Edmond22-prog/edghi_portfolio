39ced8f7b7d9e38d20958b12e61a6916cc2276d7
Edomondo <edghimakoll@gmail.com>
2024-04-20 15:11:28 +0100

    Integrate resume section template

--- website/views/base.py
+++ website/views/base.py
@@ -1,11 +1,21 @@
 from django.shortcuts import render
 
 from website.models.biography import Biography
+from website.models.resume import Resume, ResumeFile
 
 
 def base(request):
+    context = {}
     biography = Biography.objects.first()
-    if biography is not None:
-        return render(request, "base.html", {"biography": biography})
+    if biography:
+        context["biography"] = biography
     
-    return render(request, "base.html")
+    resume = Resume.objects.filter(display=True).order_by("position")
+    if resume:
+        context["resume"] = resume
+    
+    resume_file = ResumeFile.objects.all().order_by("version").last()
+    if resume_file:
+        context["resume_file"] = resume_file.get_absolute_url()
+        
+    return render(request, "base.html", context)
